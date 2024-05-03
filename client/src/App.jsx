f3a83ee9a90bcbac37a40b83918285c397e7f3ae
Darshan <darshanpatel2923@gmail.com>
2024-05-03 20:52:29 +0530

    Created structure of client side for the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import AdminDashboard from './pages/AdminDashboard'
+import EmployeeDashboard from './pages/EmployeeDashboard'
 
 const App = () => {
+  const [openCreateTask, setOpenCreateTask] = useState(false);
   return (
     <div>
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={
+          role === "admin" ? (<AdminDashboard />) : (<EmployeeDashboard setOpenCreateTask={setOpenCreateTask} />)
+        } />
+      </Routes>
+      <Footer />
     </div>
   )
 }
